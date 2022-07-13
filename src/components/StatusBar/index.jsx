681abb174bbc97c4cf2aaf2f16a5eb16b0322827
Nipun Thennakoon <nipunsampath@gmail.com>
2022-07-14 03:43:23 +0530

    Styling status bar.

--- src/components/StatusBar/index.jsx
+++ src/components/StatusBar/index.jsx
@@ -3,7 +3,7 @@ import Countdown from "react-countdown";
 const StatusBar = props => {
     const {message, isEnded, handleTimerCompletion, timerKey, initialDate} = props;
     return (
-        <div className=" grid place-items-center h-8 font-bold dark:text-white">
+        <div className=" pb-3 grid place-items-center font-bold dark:text-white text-lg">
             {isEnded ? message : <Countdown date={initialDate} onComplete={handleTimerCompletion} key={timerKey}/>}
         </div>
     );
