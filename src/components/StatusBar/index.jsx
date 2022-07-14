13772133173c6efb82f758bb3df2d124c31a3f92
Nipun Thennakoon <nipunsampath@gmail.com>
2022-07-14 15:07:27 +0530

    Added a filler div to avoid element shifting when message is empty.

--- src/components/StatusBar/index.jsx
+++ src/components/StatusBar/index.jsx
@@ -17,7 +17,7 @@ const StatusBar = props => {
     const {message, isEnded, handleTimerCompletion, timerKey, initialDate} = props;
     return (
         <div className=" pb-3 grid place-items-center font-bold dark:text-white text-lg">
-            {isEnded ? message : <Countdown date={initialDate} onComplete={handleTimerCompletion} key={timerKey} renderer={countDownRenderer}/>}
+            {isEnded ? (message ? message : <div className="h-7"/>) : <Countdown date={initialDate} onComplete={handleTimerCompletion} key={timerKey} renderer={countDownRenderer}/>}
         </div>
     );
 };
