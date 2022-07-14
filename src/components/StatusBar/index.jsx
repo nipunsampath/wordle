d4c80fee981c6417cc1d602f494e9a254779dad8
Nipun Thennakoon <nipunsampath@gmail.com>
2022-07-14 11:06:00 +0530

    Implemented a custom renderer for countdown.

--- src/components/StatusBar/index.jsx
+++ src/components/StatusBar/index.jsx
@@ -1,10 +1,23 @@
 import Countdown from "react-countdown";
 
+const countDownRenderer = ({minutes, seconds}) => {
+    let style = "";
+    if (minutes === 0 && seconds < 30){
+        style = " text-rose-600"
+    }
+    return (
+        <span className={style}>
+            {minutes.toString().padStart(2, "0")}:{seconds.toString().padStart(2, "0")}
+        </span>
+    );
+
+}
+
 const StatusBar = props => {
     const {message, isEnded, handleTimerCompletion, timerKey, initialDate} = props;
     return (
         <div className=" pb-3 grid place-items-center font-bold dark:text-white text-lg">
-            {isEnded ? message : <Countdown date={initialDate} onComplete={handleTimerCompletion} key={timerKey}/>}
+            {isEnded ? message : <Countdown date={initialDate} onComplete={handleTimerCompletion} key={timerKey} renderer={countDownRenderer}/>}
         </div>
     );
 };
